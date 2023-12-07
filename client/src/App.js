1e2b05c04a2298da0f2479f7ebbf445f16e8f7fe
김준혁 <j_hyuk2@naver.com>
2023-12-07 19:19:44 +0900

    React-Router-Dom 기초 라우팅 설정 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+
+    <div className="APP">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
