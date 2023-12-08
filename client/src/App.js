78f4f3f84c8381f0a41f4b5918c786e42c2685eb
김준혁 <j_hyuk2@naver.com>
2023-12-08 18:53:02 +0900

    axios,Proxy 까지 설치 완료, 정상 작동함

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import LandingPage from './components/views/LandingPage/LandingPage'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,17 @@
 import React from 'react'
+// {useEffect}
+// import axios from 'axios'
 
 function LandingPage() {
+
+  // /api/hello로 접근이 되었을 때 응답을 주겠다.
+  // useEffect(()=>{
+  //   axios.get('/api/hello')
+  //   .then(response=> {console.log(response.data)})
+  // },[])
+
+
+
   return (
     <div>
       LandingPage
